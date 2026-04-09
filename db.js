f93d107f15468646986f0f330daec38dd138ad83
villo33 <romeroovidio02@yahoo.com>
2026-04-09 05:16:23 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,20 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1003645363", // 👈 TU CONTRASEÑA
-    database: "mision_celestial"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
     if (err) {
-        console.log("❌ Error de conexión a MySQL:");
+        console.log("❌ Error de conexión:");
         console.log(err);
         return;
     }
-    console.log("✅ MySQL conectado correctamente");
+    console.log("✅ Conectado a Railway MySQL");
 });
 
 module.exports = db;
